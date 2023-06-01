be274a240b82e27df021c4a67eed4048e82a7b93
rangoeltango <45980284+rangoeltango@users.noreply.github.com>
2023-06-01 12:45:39 -0400

    Update Open_Read_Files.py
    Signed-off-by: rangoeltango <45980284+rangoeltango@users.noreply.github.com>

--- Open_Read_Files.py
+++ Open_Read_Files.py
@@ -8,24 +8,35 @@ import json
 with open('FlashCardQuestions.json') as json_file:
     data = json.load(json_file)
 
-'''print('This file has the following questions:\n')
-for question in data.keys():
-    print('%s' % question)
-'''
+### This section will grab the topics from each question and print them out from a de-duped list
+print('\nWelcome! We have the following topics to choose from:\n')
+available_topics = []
 
-#for keys, values in data.items():
-#    print(values[0])
+# Getting the topics from each question
+for question_info in data.values():
+    topic = question_info['topic']
+    available_topics.append(topic)
 
-for i in data['question1']:
-        print("Name:", i['name'])
+# Creating a de-duped list by converting into dictionary (which cannot have duplicate keys) first, then back to list
+de_duped_topics = list(dict.fromkeys(available_topics))
 
-'''
-    # For reading nested data [0] represents the index value of the list
-    print(data['question2'][0])
+# Printing the (unique) topics available
+for topic in de_duped_topics:
+    print(topic)
 
-    # For printing the key-value pair of nested dictionary FOR loop can be used
-    print("\nPrinting nested dictionary as a key-value pair\n")
-    for i in data['question1']:
-        print("Name:", i['name'])
-'''
+# Asking user which topic they would like to study
+user_input = input('\nWhich topic would you like to study?')
+
+### This section will create a temporary set of flash cards based on the user's topic selection
+flashcards = []
+
+for question, question_info in data.items():
+    topic = question_info['topic']
+    if user_input == topic:
+        flashcards.append(question_info['question'])
+    else:
+        continue
+
+for question in flashcards:
+    print(question)
 
